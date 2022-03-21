da2b36c303b2e53a6a45762276cef7cc73f43c68
CaMiMtoto <jeanpaulcami@live.com>
2022-03-21 12:37:52 +0200

    disable ddl-auto

---
+++ data.sql
@@ -0,0 +1,19 @@
+INSERT INTO public.drivers (id, created_at, updated_at, email, is_available, latitude, longitude, name, phone) VALUES (1, '2022-03-21 11:57:14.672000', '2022-03-21 11:57:14.672000', 'rhiannon.willms@yahoo.com', true, '-1.956537', '30.063616', 'Edison Stoltenberg Jr.', '720-418-4484');
+INSERT INTO public.drivers (id, created_at, updated_at, email, is_available, latitude, longitude, name, phone) VALUES (2, '2022-03-21 11:57:14.697000', '2022-03-21 11:57:14.697000', 'marcellus.littel@gmail.com', true, '-1.971142', '30.103683', 'Miss Roxane Runte', '(335) 974-5692');
+INSERT INTO public.drivers (id, created_at, updated_at, email, is_available, latitude, longitude, name, phone) VALUES (3, '2022-03-21 11:57:14.698000', '2022-03-21 11:57:14.698000', 'dorcas.marvin@yahoo.com', false, '-1.949549', '30.126161', 'Ignatius Kiehn', '631.714.9496');
+INSERT INTO public.drivers (id, created_at, updated_at, email, is_available, latitude, longitude, name, phone) VALUES (4, '2022-03-21 11:57:14.700000', '2022-03-21 11:57:14.700000', 'ova.sawayn@gmail.com', true, '-1.978963', '30.223335', 'Mariela Windler', '1-750-175-2709');
+INSERT INTO public.drivers (id, created_at, updated_at, email, is_available, latitude, longitude, name, phone) VALUES (5, '2022-03-21 11:57:14.703000', '2022-03-21 11:57:14.703000', 'jewell.hahn@gmail.com', true, '-1.97794', '30.043773', 'Joan Wolf', '880-407-5409');
+
+
+
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (1, '2022-03-21 11:57:15.127000', '2022-03-21 11:57:15.127000', 'kane.erdman@yahoo.com', 'D''angelo Greenholt', '916-425-8888 x333');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (2, '2022-03-21 11:57:15.132000', '2022-03-21 11:57:15.132000', 'lillian.gleichner@gmail.com', 'Jarod Hoeger', '(574) 858-3333');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (3, '2022-03-21 11:57:15.137000', '2022-03-21 11:57:15.137000', 'darrion.jast@hotmail.com', 'Mrs. Andres Olson', '1-816-216-5555 x555');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (4, '2022-03-21 11:57:15.141000', '2022-03-21 11:57:15.141000', 'brady.blick@yahoo.com', 'Ms. Rosie Wilkinson', '1-678-978-7777');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (5, '2022-03-21 11:57:15.145000', '2022-03-21 11:57:15.145000', 'jorge.leannon@hotmail.com', 'Corine Upton', '1-862-443-2222 x777');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (6, '2022-03-21 11:57:15.150000', '2022-03-21 11:57:15.150000', 'kaylah.dach@hotmail.com', 'Dr. Zora Hickle', '1-970-318-9999 x444');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (7, '2022-03-21 11:57:15.156000', '2022-03-21 11:57:15.156000', 'brennan.beer@hotmail.com', 'Kameron Wunsch', '(651) 310-0000');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (8, '2022-03-21 11:57:15.160000', '2022-03-21 11:57:15.160000', 'margret.blick@gmail.com', 'Madaline Leuschke', '1-630-602-8888 x777');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (9, '2022-03-21 11:57:15.164000', '2022-03-21 11:57:15.164000', 'melany.pollich@yahoo.com', 'Walker Dietrich', '530.757.7777 x000');
+INSERT INTO public.riders (id, created_at, updated_at, email, name, phone) VALUES (10, '2022-03-21 11:57:15.169000', '2022-03-21 11:57:15.169000', 'danyka.wolff@yahoo.com', 'Johathan Watsica', '574.740.0000 x444');
+
